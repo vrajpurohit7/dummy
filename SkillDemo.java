4b35fabdf3bcb2a43fb794e33c323396671497ce
vrajpurohit7 <vrajpurohit@ucsd.edu>
2022-04-27 11:45:29 -0700

    edits

---
+++ SkillDemo.java
@@ -0,0 +1,5 @@
+public class SkillDemo{
+    public static int avg(int a, int b) {
+        return (a+b)/2;
+    }
+}
---
+++ SkillDemoTester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+    
+    @Test
+    public void average() {
+        assertEquals(1, SkillDemo.avg(1,1));
+    }
+}
